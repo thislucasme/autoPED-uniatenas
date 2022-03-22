e4261b31b69e7f289504601a1f26e1dab24c661b
Lucas Silva <lucassilvaee1245@gmail.com>
2022-03-21 21:48:50 -0300

    endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,21 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 @Controller('auth')
 export class AuthController {
 
-    @Post('login')
+    @Post('login-coordenador')
     async login (@Body() body:Usuario){
-        return {hell0o: "ehejdhjdhsdghfgh"};
+        return null;
     }
-
+    @Post('login-ti')
+    async loginTI (@Body() body:Usuario){
+        return null;
+    }
+    @Post('cadastro-coordenador')
+    async cadastroCoordenador (@Body() body:Coordenador){
+        return null;
+    }
     @Get()
-    async getUser(){
-        return {hell0o: "ehejdhjdhsdghfgh"};
+    async getUser(@Body() coordenador:Coordenador){
+        return null;
     }
 
     
--- src/models/types.ts
+++ src/models/types.ts
@@ -2,3 +2,16 @@ interface Usuario {
   email:string,
   senha:string
 }
+
+interface Coordenador {
+  nome:string,
+  email:string,
+  semha:string
+}
+
+interface UsuarioMaster {
+  nome:string,
+  email:string,
+  semha:string
+}
+
